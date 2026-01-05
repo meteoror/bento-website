7fe6f93da71b4181d20204ece471ba01741b761a
meteoror <crashdown13542@gmail.com>
2026-01-04 23:19:38 -0500

    removed all excess debug stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Switch back to main App
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
