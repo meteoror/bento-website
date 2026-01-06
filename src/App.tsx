7ff0f50af14063273a61eabc638ff1459d14dbb0
meteoror <crashdown13542@gmail.com>
2026-01-06 18:06:43 -0500

    added ugly

--- src/App.tsx
+++ src/App.tsx
@@ -63,6 +63,8 @@ const App: React.FC = () => {
               <p>
                 i like griefer (blocktales) clonelin (collinlock16) and broker (phighting!) a Normal amount. i dont rlly like saying im nonsharing but i do yume them and am kind of insane about them on socials sometimes, i will prob moot u up if you like any of those medias (except phighting sorry the community sucks so ass) i also like bumblebee (transformers) subspace (phighting) ire (grace) and 007n7 (myth community)... i'm very open to new medias :-)
               </p>
+              <p> my chud son </p>
+              <a href="https://www.chickensmoothie.com/pet/503053035.html"><img src="https://www.chickensmoothie.com/pet/503053035&trans=1.jpg" border=0></a><br><b>Pet's name:</b> exemption <br><a href="https://www.chickensmoothie.com/">Adopt virtual pets at Chicken Smoothie!</a>
             </div>
           </section>
 
